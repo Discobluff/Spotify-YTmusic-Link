b5faf2f1d578f08b3eda3476dc2a667f1c2ca8d7
Hugo Dartoy <hugo.dartoy@telecomnancy.eu>
2026-02-13 11:19:25 +0100

    fix(spotify api): remove useless headers

--- api/spotify.py
+++ api/spotify.py
@@ -10,24 +10,8 @@ url = "https://api-partner.spotify.com/pathfinder/v2/query"
 
 def getHeaders():
     headers = {
-        "Host": "api-partner.spotify.com",
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:142.0) Gecko/20100101 Firefox/142.0",
-        "Accept": "application/json",
-        "Accept-Language": "en",
-        "Accept-Encoding": "gzip, deflate, br, zstd",
-        "Content-Type": "application/json;charset=UTF-8",
-        "Referer": "https://open.spotify.com/",
-        "app-platform": "WebPlayer",
-        "spotify-app-version": "1.2.84.193.gd6194a30",
-        "client-token": clientToken,
-        "Origin": "https://open.spotify.com",
-        "Sec-GPC": "1",
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "same-site",
         "Authorization": f"Bearer {accessToken}",
-        "Connection": "keep-alive",
-        "TE": "trailers"
     }
     return headers
 
